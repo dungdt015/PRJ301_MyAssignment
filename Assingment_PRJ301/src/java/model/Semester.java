10e3d989eda56393adafa30a433930dda9dcb993
dungdt015 <dungdotn0105@gmail.com>
2024-06-26 14:33:37 +0700

    create folder model

---
+++ Assingment_PRJ301/src/java/model/Semester.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author admin
+ */
+public class Semester {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+   
+    
+}
